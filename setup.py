1c1f0e6c9809ac6ad34de7713b6b971dcaee3463
Ansh Darji <anshdarji96@gmail.com>
2024-05-30 21:37:52 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description="MLOPS package",
+    author = "Ansh",
+    packages= find_packages(),
+    license="MIT",
+    author_email="anshdarji@gmail.com"
+
+
+)
